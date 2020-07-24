bd60914743acfdeae7311b5c8d494d0aaaa904ab
Vadim Moroz <vmoroz13@yandex.ru>
2020-07-24 15:04:52 +0300

    main

--- src/modules/calcAccordion.js
+++ src/modules/calcAccordion.js
@@ -37,7 +37,7 @@ const calcAccordion = () => {
         console.log(e);
         e.addEventListener('click', (event) => {
             const target = event.target;
-           // console.log(target);
+           console.log(target);
         });
     });
 
